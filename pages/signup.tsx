2c6032d790a5576d03483fedf9e7f2c2f6a349ac
Jakub <zielvna@gmail.com>
2022-09-17 17:12:06 +0200

    create signup page

---
+++ pages/signup.tsx
@@ -0,0 +1,35 @@
+import type { NextPage } from 'next';
+import { RiCamera2Line } from 'react-icons/ri';
+
+import Wrapper from '../components/Wrapper';
+import Card from '../components/Card';
+import Input from '../components/Input';
+import Button from '../components/Button';
+import Link from '../components/Link';
+
+const SignUp: NextPage = () => {
+    return (
+        <Wrapper>
+            <Card className="mt-20">
+                <div className="flex justify-center">
+                    <RiCamera2Line className="text-3xl text-black" />
+                </div>
+                <hr className="text-light-gray my-4" />
+                <form className="max-w-xs">
+                    <Input type="text" name="username" placeholder="Username" spellCheck="false" />
+                    <Input className="mt-2" type="email" name="email" placeholder="E-mail" spellCheck="false" />
+                    <Input className="mt-2" type="password" name="password" placeholder="Password" />
+                    <Button className="w-full mt-4">Sign up</Button>
+                </form>
+                <hr className="text-light-gray my-4" />
+                <div className="flex justify-center">
+                    <p>
+                        Have an account? <Link href="/login">Log in</Link>
+                    </p>
+                </div>
+            </Card>
+        </Wrapper>
+    );
+};
+
+export default SignUp;

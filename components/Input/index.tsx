7552c4d826d277f04a1171cdec255e50c6280b24
Jakub <zielvna@gmail.com>
2022-09-17 16:59:21 +0200

    fix input responsiveness

--- components/Input/index.tsx
+++ components/Input/index.tsx
@@ -6,7 +6,7 @@ type Props = {
 };
 
 const Input = ({ className = '', ...props }: Props) => (
-    <input className={`p-2 bg-light-gray rounded-lg outline-0 ${className}`} {...props} />
+    <input className={`w-full p-2 bg-light-gray rounded-lg outline-0 ${className}`} {...props} />
 );
 
 export default Input;

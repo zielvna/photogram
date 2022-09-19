1d34425e68dddb8e8288099004d60f3a6e8052d2
Jakub <zielvna@gmail.com>
2022-09-19 20:12:50 +0200

    change wrapper padding

--- components/Wrapper/index.tsx
+++ components/Wrapper/index.tsx
@@ -3,7 +3,7 @@ type Props = {
 };
 
 const Wrapper = ({ children }: Props) => (
-    <div className="max-w-5xl mx-auto px-2 flex flex-col items-center">{children}</div>
+    <div className="max-w-5xl mx-auto px-4 flex flex-col items-center">{children}</div>
 );
 
 export default Wrapper;

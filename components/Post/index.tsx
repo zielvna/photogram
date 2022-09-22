a54953e791f9d217bdc4bb3d1bc5b896f49d3d3a
Jakub <zielvna@gmail.com>
2022-09-22 18:58:59 +0200

    remove unnecessary styles from post component

--- components/Post/index.tsx
+++ components/Post/index.tsx
@@ -12,46 +12,44 @@ type Props = {
 };
 
 const Post = ({ scheme = 'normal' }: Props) => (
-    <div className={classnames('max-w-2xl', { 'md:max-w-5xl': scheme === 'normal' })}>
-        <Card>
-            <div className={classnames({ 'md:flex': scheme === 'normal' })}>
-                <div className={classnames({ 'md:hidden': scheme === 'normal' })}>
-                    <PostComment />
-                </div>
-                <div className={classnames('mt-4', { 'md:mt-0': scheme === 'normal' })}>
-                    <NextImage className="rounded-lg" src="/640.png" width="640" height="640" alt="Post image." />
-                </div>
-                {scheme === 'normal' && (
-                    <div className="mt-4 flex flex-col justify-between shrink-0 md:w-80 md:mt-0 md:pl-4">
-                        <div className="hidden md:block">
-                            <PostComment />
-                            <hr className="text-light-gray my-4" />
-                        </div>
-                        <div className="grow overflow-y-scroll max-h-80 md:h-0 md:max-h-max">
-                            <PostComment />
-                            <div className="mt-4">
-                                <PostComment />
-                            </div>
-                        </div>
-                        <hr className="text-light-gray my-4" />
-                        <PostActions />
-                        <hr className="text-light-gray my-4" />
-                        <div className="flex items-center">
-                            <Input placeholder="Add a comment..." />
-                            <div className="ml-2">
-                                <Button scheme="small">Post</Button>
-                            </div>
-                        </div>
-                    </div>
-                )}
-                {scheme === 'preview' && (
-                    <div className="mt-4">
-                        <PostActions />
-                    </div>
-                )}
+    <Card>
+        <div className={classnames({ 'md:flex': scheme === 'normal' })}>
+            <div className={classnames({ 'md:hidden': scheme === 'normal' })}>
+                <PostComment />
             </div>
-        </Card>
-    </div>
+            <div className={classnames('mt-4', { 'md:mt-0': scheme === 'normal' })}>
+                <NextImage className="rounded-lg" src="/640.png" width="640" height="640" alt="Post image." />
+            </div>
+            {scheme === 'normal' && (
+                <div className="mt-4 flex flex-col justify-between shrink-0 md:w-80 md:mt-0 md:pl-4">
+                    <div className="hidden md:block">
+                        <PostComment />
+                        <hr className="text-light-gray my-4" />
+                    </div>
+                    <div className="grow overflow-y-scroll max-h-80 md:h-0 md:max-h-max">
+                        <PostComment />
+                        <div className="mt-4">
+                            <PostComment />
+                        </div>
+                    </div>
+                    <hr className="text-light-gray my-4" />
+                    <PostActions />
+                    <hr className="text-light-gray my-4" />
+                    <div className="flex items-center">
+                        <Input placeholder="Add a comment..." />
+                        <div className="ml-2">
+                            <Button scheme="small">Post</Button>
+                        </div>
+                    </div>
+                </div>
+            )}
+            {scheme === 'preview' && (
+                <div className="mt-4">
+                    <PostActions />
+                </div>
+            )}
+        </div>
+    </Card>
 );
 
 export default Post;

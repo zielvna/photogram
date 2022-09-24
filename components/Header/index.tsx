3f519856dc96268f5ff3b4cd397e7d14cfd985be
Jakub <zielvna@gmail.com>
2022-09-24 02:36:54 +0200

    fix search and header component styling

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -44,44 +44,51 @@ const Header = () => {
     }
 
     return (
-        <header className="h-16 flex justify-center bg-white">
-            <div className="w-full max-w-5xl px-4 flex items-center justify-between">
-                <div className={classnames('w-44', { 'hidden md:block': isSearchOpen })}>
-                    <Link href="/">
-                        <RiCamera2Line className="text-3xl text-black cursor-pointer" />
-                    </Link>
-                </div>
-                <div
-                    className={classnames('flex items-center md:w-80 md:relative', { 'w-full': isSearchOpen })}
-                    ref={searchRef}
-                >
-                    <div className={classnames('w-full md:block', { hidden: !isSearchOpen })}>
-                        <Input name="search" placeholder="Search" spellCheck="false" onClick={openSearch} />
+        <header className="relative z-10">
+            <div
+                className={classnames('h-16 flex justify-center bg-white', { 'fixed inset-0 md:static': isSearchOpen })}
+            >
+                <div className="w-full max-w-5xl px-4 flex items-center justify-between">
+                    <div className={classnames('w-44', { 'hidden md:block': isSearchOpen })}>
+                        <Link href="/">
+                            <RiCamera2Line className="text-3xl text-black cursor-pointer" />
+                        </Link>
                     </div>
-                    <RiCloseLine
-                        className={classnames('ml-2 text-3xl text-black shrink-0 cursor-pointer md:hidden', {
-                            hidden: !isSearchOpen,
-                        })}
-                        onClick={closeSearch}
-                    />
-                    <Search isOpen={isSearchOpen} />
-                </div>
-                <div className={classnames('w-44 flex justify-end', { 'hidden md:flex': isSearchOpen })}>
-                    <RiSearchLine className="mx-2 text-3xl text-black cursor-pointer md:hidden" onClick={openSearch} />
-                    <Link href="/">
-                        <RiHomeLine className="mx-2 text-3xl text-black cursor-pointer" />
-                    </Link>
-                    <Link href="/new-post">
-                        <RiAddBoxLine className="mx-2 text-3xl text-black cursor-pointer" />
-                    </Link>
-                    <div className="md:relative">
-                        <RiUserLine className="ml-2 text-3xl text-black cursor-pointer" onClick={openDropdown} />
-                        <Dropdown
-                            show={isDropdownOpen}
-                            items={['Profile', 'Settings', 'Logout']}
-                            onChange={handleChange}
-                            passRef={dropdownRef}
+                    <div
+                        className={classnames('flex items-center md:w-80 md:relative z-10', { 'w-full': isSearchOpen })}
+                        ref={searchRef}
+                    >
+                        <div className={classnames('w-full md:block', { hidden: !isSearchOpen })}>
+                            <Input name="search" placeholder="Search" spellCheck="false" onClick={openSearch} />
+                        </div>
+                        <RiCloseLine
+                            className={classnames('ml-2 text-3xl text-black shrink-0 cursor-pointer md:hidden', {
+                                hidden: !isSearchOpen,
+                            })}
+                            onClick={closeSearch}
                         />
+                        <Search isOpen={isSearchOpen} />
+                    </div>
+                    <div className={classnames('w-44 flex justify-end', { 'hidden md:flex': isSearchOpen })}>
+                        <RiSearchLine
+                            className="mx-2 text-3xl text-black cursor-pointer md:hidden"
+                            onClick={openSearch}
+                        />
+                        <Link href="/">
+                            <RiHomeLine className="mx-2 text-3xl text-black cursor-pointer" />
+                        </Link>
+                        <Link href="/new-post">
+                            <RiAddBoxLine className="mx-2 text-3xl text-black cursor-pointer" />
+                        </Link>
+                        <div className="md:relative">
+                            <RiUserLine className="ml-2 text-3xl text-black cursor-pointer" onClick={openDropdown} />
+                            <Dropdown
+                                show={isDropdownOpen}
+                                items={['Profile', 'Settings', 'Logout']}
+                                onChange={handleChange}
+                                passRef={dropdownRef}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
--- components/Search/index.tsx
+++ components/Search/index.tsx
@@ -7,7 +7,7 @@ type Props = {
 const Search = ({ isOpen }: Props) => (
     <div
         className={classnames(
-            'w-full bg-white px-2 rounded-lg absolute inset-0 top-16 md:p-2 md:border md:border-light-gray md:top-full md:inset-auto',
+            'w-full bg-white px-2 rounded-lg fixed inset-0 top-16 md:p-2 md:border md:border-light-gray md:absolute md:top-full md:inset-auto',
             { hidden: !isOpen }
         )}
     ></div>

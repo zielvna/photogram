e461eab69d7ac7f078f4dbc4abbe89d4558db788
Jakub <zielvna@gmail.com>
2022-09-24 02:10:00 +0200

    change padding of items in dropdown component

--- components/Dropdown/index.tsx
+++ components/Dropdown/index.tsx
@@ -23,12 +23,16 @@ const Dropdown = ({ show, items, passRef, onChange }: Props) => {
             <div className="w-full bg-white border border-light-gray self-center rounded-lg cursor-pointer">
                 <div ref={passRef}>
                     {items.map((name) => (
-                        <div className="p-2 border-b border-light-gray" key={name} onClick={() => itemClick(name)}>
+                        <div
+                            className="py-2 px-4 border-b border-light-gray"
+                            key={name}
+                            onClick={() => itemClick(name)}
+                        >
                             {name}
                         </div>
                     ))}
                 </div>
-                <div className="p-2 block">Close</div>
+                <div className="py-2 px-4 block">Close</div>
             </div>
         </div>
     );

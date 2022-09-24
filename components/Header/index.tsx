713dc0f8e0d4854890af53c7f3250f76498865cf
Jakub <zielvna@gmail.com>
2022-09-24 02:11:39 +0200

    set dropdown and links in header component

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,16 +1,24 @@
 import { useRef, useState } from 'react';
+import { useRouter } from 'next/router';
 import classnames from 'classnames';
 import { RiCamera2Line, RiCloseLine, RiSearchLine, RiHomeLine, RiAddBoxLine, RiUserLine } from 'react-icons/ri';
 
+import Link from '../Link';
+import useDropdown from '../../hooks/useDropdown';
 import useOnClickOutside from '../../hooks/useOnClickOutside';
 import Input from '../Input';
 import Search from '../Search';
+import Dropdown from '../Dropdown';
 
 const Header = () => {
-    const ref = useRef<HTMLDivElement>(null);
+    const searchRef = useRef<HTMLDivElement>(null);
+    const dropdownRef = useRef<HTMLDivElement>(null);
     const [isSearchOpen, setIsSearchOpen] = useState(false);
+    const [isDropdownOpen, openDropdown, closeDropdown] = useDropdown(dropdownRef);
 
-    useOnClickOutside(ref, () => {
+    const router = useRouter();
+
+    useOnClickOutside(searchRef, () => {
         setIsSearchOpen(false);
     });
 
@@ -22,15 +30,30 @@ const Header = () => {
         setIsSearchOpen(false);
     }
 
+    function handleChange(name: string) {
+        switch (name) {
+            case 'Profile':
+                router.push('/user/user');
+                break;
+            case 'Settings':
+                router.push('/settings/edit-profile');
+                break;
+        }
+
+        closeDropdown();
+    }
+
     return (
         <header className="h-16 flex justify-center bg-white">
             <div className="w-full max-w-5xl px-4 flex items-center justify-between">
                 <div className={classnames('w-44', { 'hidden md:block': isSearchOpen })}>
-                    <RiCamera2Line className="text-3xl text-black cursor-pointer" />
+                    <Link href="/">
+                        <RiCamera2Line className="text-3xl text-black cursor-pointer" />
+                    </Link>
                 </div>
                 <div
                     className={classnames('flex items-center md:w-80 md:relative', { 'w-full': isSearchOpen })}
-                    ref={ref}
+                    ref={searchRef}
                 >
                     <div className={classnames('w-full md:block', { hidden: !isSearchOpen })}>
                         <Input name="search" placeholder="Search" spellCheck="false" onClick={openSearch} />
@@ -45,9 +68,21 @@ const Header = () => {
                 </div>
                 <div className={classnames('w-44 flex justify-end', { 'hidden md:flex': isSearchOpen })}>
                     <RiSearchLine className="mx-2 text-3xl text-black cursor-pointer md:hidden" onClick={openSearch} />
-                    <RiHomeLine className="mx-2 text-3xl text-black cursor-pointer" />
-                    <RiAddBoxLine className="mx-2 text-3xl text-black cursor-pointer" />
-                    <RiUserLine className="ml-2 text-3xl text-black cursor-pointer" />
+                    <Link href="/">
+                        <RiHomeLine className="mx-2 text-3xl text-black cursor-pointer" />
+                    </Link>
+                    <Link href="/new-post">
+                        <RiAddBoxLine className="mx-2 text-3xl text-black cursor-pointer" />
+                    </Link>
+                    <div className="md:relative">
+                        <RiUserLine className="ml-2 text-3xl text-black cursor-pointer" onClick={openDropdown} />
+                        <Dropdown
+                            show={isDropdownOpen}
+                            items={['Profile', 'Settings', 'Logout']}
+                            onChange={handleChange}
+                            passRef={dropdownRef}
+                        />
+                    </div>
                 </div>
             </div>
         </header>

33acbc28140f24d57ccea79b6041c257b3d98c10
Jakub <zielvna@gmail.com>
2022-09-30 01:34:50 +0200

    wrap app with user provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import type { AppProps } from 'next/app';
 
+import '../firebase.ts';
+import { UserProvider } from '../contexts/userContext';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <UserProvider>
+            <Component {...pageProps} />
+        </UserProvider>
+    );
 }
 
 export default MyApp;

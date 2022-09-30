5aef9f1fbdae6bbf602ab4b0b57157caacc83056
Jakub <zielvna@gmail.com>
2022-09-30 02:41:26 +0200

    add register and validation props to input

--- components/Input/index.tsx
+++ components/Input/index.tsx
@@ -1,9 +1,16 @@
 /* eslint-disable  @typescript-eslint/no-explicit-any */
 
+import { UseFormRegister, FieldValues } from 'react-hook-form';
+
 type Props = {
+    name: string;
+    register: UseFormRegister<FieldValues>;
+    validation?: object;
     [props: string]: any;
 };
 
-const Input = ({ ...props }: Props) => <input className="w-full p-2 bg-light-gray rounded-lg outline-0" {...props} />;
+const Input = ({ name, register, validation = {}, ...props }: Props) => (
+    <input className="w-full p-2 bg-light-gray rounded-lg outline-0" {...register(name, validation)} {...props} />
+);
 
 export default Input;

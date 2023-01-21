043fdace763f12c14ddd37aad6e0f427a6650d35
Jakub <zielvna@gmail.com>
2023-01-21 13:33:02 +0100

    add storage in firebase config

--- firebase.ts
+++ firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -14,3 +15,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 getAuth(app);
 getFirestore(app);
+getStorage(app);

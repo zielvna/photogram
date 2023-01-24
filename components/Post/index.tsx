13c95f0a6ce07f7baf7908668144f29fccbc6103
Jakub <zielvna@gmail.com>
2023-01-24 19:06:31 +0100

    add logic to post page

--- components/Post/index.tsx
+++ components/Post/index.tsx
@@ -1,29 +1,34 @@
 import NextImage from 'next/future/image';
 import classnames from 'classnames';
 
+import IPost from '../../types/Post';
+import IUser from '../../types/User';
 import Card from '../Card';
+import PostDescription from './PostDescription';
 import PostComment from './PostComment';
 import PostActions from './PostActions';
 import Input from '../Input';
 import Button from '../Button';
 
 type Props = {
+    post: IPost;
+    author: IUser;
     scheme: 'normal' | 'preview';
 };
 
-const Post = ({ scheme = 'normal' }: Props) => (
+const Post = ({ post, author, scheme = 'normal' }: Props) => (
     <Card>
         <div className={classnames({ 'md:flex': scheme === 'normal' })}>
             <div className={classnames({ 'md:hidden': scheme === 'normal' })}>
-                <PostComment />
+                <PostDescription user={author} content={post.description} timestamp={post.timestamp} />
             </div>
             <div className={classnames('mt-4', { 'md:mt-0': scheme === 'normal' })}>
-                <NextImage className="rounded-lg" src="/640.png" width="640" height="640" alt="Post image." />
+                <NextImage className="rounded-lg" src={post.photoUrl} width="640" height="640" alt="Post image." />
             </div>
             {scheme === 'normal' && (
                 <div className="mt-4 flex flex-col justify-between shrink-0 md:w-80 md:mt-0 md:pl-4">
                     <div className="hidden md:block">
-                        <PostComment />
+                        <PostDescription user={author} content={post.description} timestamp={post.timestamp} />
                         <hr className="text-light-gray my-4" />
                     </div>
                     <div className="grow overflow-y-scroll max-h-80 md:h-0 md:max-h-max">
@@ -36,7 +41,7 @@ const Post = ({ scheme = 'normal' }: Props) => (
                     <PostActions />
                     <hr className="text-light-gray my-4" />
                     <div className="flex items-center">
-                        <Input placeholder="Add a comment..." />
+                        <Input name="comment" placeholder="Add a comment..." />
                         <div className="ml-2">
                             <Button scheme="small">Post</Button>
                         </div>
--- pages/post/[postId].tsx
+++ pages/post/[postId].tsx
@@ -1,20 +1,36 @@
-import type { NextPage } from 'next';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 
+import IPost from '../../types/Post';
+import IUser from '../../types/User';
+import { getPost, getUser } from '../../functions';
 import Header from '../../components/Header';
 import Wrapper from '../../components/Wrapper';
 import Post from '../../components/Post';
 
-const PostPage: NextPage = () => {
-    return (
-        <>
-            <Header />
-            <Wrapper>
-                <div className="mt-4">
-                    <Post scheme="normal" />
-                </div>
-            </Wrapper>
-        </>
-    );
+export const getServerSideProps: GetServerSideProps<{ post: IPost; author: IUser }> = async (context) => {
+    const postId = context.params?.postId;
+
+    const post = await getPost(postId as string);
+
+    const author = await getUser(post.userId);
+
+    return {
+        props: {
+            post,
+            author,
+        },
+    };
 };
+
+const PostPage = ({ post, author }: InferGetServerSidePropsType<typeof getServerSideProps>) => (
+    <>
+        <Header />
+        <Wrapper>
+            <div className="mt-4">
+                {post ? <Post post={post} author={author} scheme="normal" /> : <p>Post not found.</p>}
+            </div>
+        </Wrapper>
+    </>
+);
 
 export default PostPage;

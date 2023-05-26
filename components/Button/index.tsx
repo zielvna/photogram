bfbba161e9126782c0b041d935642c069c8fde17
Jakub <zielvna@gmail.com>
2023-05-26 10:40:29 +0200

    add new type in button component

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,14 +1,25 @@
 /* eslint-disable  @typescript-eslint/no-explicit-any */
 
 type Props = {
-    scheme?: 'normal' | 'small';
+    scheme?: 'normal' | 'inverse' | 'small';
     children: React.ReactNode;
     [props: string]: any;
 };
 
-const Button = ({ children, scheme = 'normal', ...props }: Props) => {
-    const buttonStyles =
-        scheme === 'normal' ? 'w-full py-2 bg-blue text-white font-bold rounded-lg' : 'text-blue font-bold';
+const Button = ({ children, scheme = 'normal', className, ...props }: Props) => {
+    let buttonStyles;
+
+    switch (scheme) {
+        case 'normal':
+            buttonStyles = 'w-full py-2 bg-blue text-white font-bold rounded-lg border border-white';
+            break;
+        case 'inverse':
+            buttonStyles = 'w-full py-2 bg-white text-blue font-bold rounded-lg border border-blue';
+            break;
+        case 'small':
+            buttonStyles = 'text-blue font-bold';
+            break;
+    }
 
     return (
         <button className={buttonStyles} {...props}>

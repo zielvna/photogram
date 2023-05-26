718834b37d476fa2dbabe6b8e68c8d4f13081c01
Jakub <zielvna@gmail.com>
2023-05-26 07:03:33 +0200

    create firebase admin file and update gitignore

---
+++ firebaseAdmin.ts
@@ -0,0 +1,20 @@
+import { credential } from 'firebase-admin';
+import { initializeApp, getApps, getApp } from 'firebase-admin/app';
+import { getAuth } from 'firebase-admin/auth';
+import serviceAccountKey from './serviceAccountKey.json';
+
+const firebaseConfig = {
+    credential: credential.cert({
+        privateKey: serviceAccountKey.private_key,
+        clientEmail: serviceAccountKey.client_email,
+        projectId: serviceAccountKey.project_id,
+    }),
+};
+
+if (!getApps().length) {
+    initializeApp(firebaseConfig);
+}
+
+const auth = getAuth(getApp());
+
+export { auth };

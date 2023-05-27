bb5cfce5f603bbd00dda244a7b0d28f9c60848c5
Jakub <zielvna@gmail.com>
2023-05-27 13:37:11 +0200

    refactor home page and post page + add pagination

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,92 @@
-import type { NextPage } from 'next';
+/* eslint-disable no-empty */
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import nookies from 'nookies';
 
+import { auth } from '../firebaseAdmin';
 import Header from '../components/Header';
 import Wrapper from '../components/Wrapper';
 import Post from '../components/Post';
+import IPost from '../types/Post';
+import { getHomePagePosts, getPost } from '../functions';
+import { useEffect, useState } from 'react';
+import useUser from '../hooks/useUser';
 
-const HomePage: NextPage = () => {
+export const getServerSideProps: GetServerSideProps<{
+    posts: IPost[];
+}> = async (context) => {
+    const cookies = nookies.get(context);
+    let loggedUserId = null;
+
+    if (cookies.token) {
+        try {
+            loggedUserId = (await auth.verifyIdToken(cookies.token)).uid;
+        } catch (error) {
+            loggedUserId = null;
+        }
+    }
+
+    const postIds = await getHomePagePosts(2);
+
+    const posts: IPost[] = [];
+
+    try {
+        for (let i = 0; i < postIds.length; i++) {
+            posts.push(await getPost(loggedUserId, postIds[i], true, false, true));
+        }
+    } catch (error) {
+        posts.splice(0, posts.length);
+    }
+
+    return {
+        props: {
+            posts,
+        },
+    };
+};
+
+const HomePage = ({ posts }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+    const user = useUser();
+    const [index, setIndex] = useState(0);
+    const [postsList, setPostsList] = useState(posts);
+    const [message, setMessage] = useState('Loading posts...');
+
+    useEffect(() => {
+        const listener = async () => {
+            if (window.innerHeight + Math.round(window.scrollY) >= document.body.offsetHeight) {
+                setIndex(index + 3);
+
+                const postIds = await getHomePagePosts(2, postsList[postsList.length - 1].timestamp);
+
+                const posts: IPost[] = [];
+
+                for (let i = 0; i < postIds.length; i++) {
+                    const post = await getPost(user?.uid ?? null, postIds[i], true, false, true);
+                    posts.push(post);
+                }
+
+                if (postIds.length < 3) {
+                    setMessage('No more posts!');
+                }
+
+                setPostsList([...postsList, ...posts]);
+            }
+        };
+
+        window.addEventListener('scroll', listener);
+
+        return () => removeEventListener('scroll', listener);
+    }, [user]);
+
     return (
         <>
             <Header />
             <Wrapper>
-                <div className="mt-4">
-                    <Post scheme="preview" />
-                </div>
+                {postsList.map((post) => (
+                    <div key={post.id} className="mt-4">
+                        <Post post={post} scheme="preview" />
+                    </div>
+                ))}
+                <div className="my-4">{message}</div>
             </Wrapper>
         </>
     );
--- pages/post/[postId].tsx
+++ pages/post/[postId].tsx
@@ -3,93 +3,46 @@ import nookies from 'nookies';
 
 import { auth } from '../../firebaseAdmin';
 import IPost from '../../types/Post';
-import IUser from '../../types/User';
-import { getComments, getLikes, getPost, getUser, isLiked } from '../../functions';
+import { getPost } from '../../functions';
 import Header from '../../components/Header';
 import Wrapper from '../../components/Wrapper';
 import Post from '../../components/Post';
-import IComment from '../../types/Comment';
 
 export const getServerSideProps: GetServerSideProps<{
-    userId: string | null;
     post: IPost | null;
-    author: IUser | null;
-    comments: IComment[] | null;
-    likeCount: number | null;
-    isPostLiked: boolean | null;
 }> = async (context) => {
-    const postId = context.params?.postId;
-
-    const post = await getPost(postId as string);
-
-    if (!post) {
-        return {
-            props: {
-                userId: null,
-                post: null,
-                author: null,
-                comments: null,
-                likeCount: null,
-                isLiked: null,
-                isPostLiked: null,
-            },
-        };
-    }
-
-    const author = await getUser(post.userId);
-    const comments = await getComments(postId as string);
-    const likeCount = await getLikes(postId as string);
-
     const cookies = nookies.get(context);
-
-    let isPostLiked = false;
-
-    let userId = null;
+    let loggedUserId = null;
 
     if (cookies.token) {
-        userId = (await auth.verifyIdToken(cookies.token)).uid;
+        try {
+            loggedUserId = (await auth.verifyIdToken(cookies.token)).uid;
+        } catch (error) {
+            loggedUserId = null;
+        }
+    }
 
-        isPostLiked = await isLiked(userId, postId as string);
+    const postId = context.params?.postId as string;
+    let post = null;
+
+    try {
+        post = await getPost(loggedUserId, postId, true, true, true);
+    } catch (error) {
+        post = null;
     }
 
     return {
         props: {
-            userId,
             post,
-            author,
-            comments,
-            likeCount,
-            isPostLiked,
         },
     };
 };
 
-const PostPage = ({
-    userId,
-    post,
-    author,
-    comments,
-    likeCount,
-    isPostLiked,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) => (
+const PostPage = ({ post }: InferGetServerSidePropsType<typeof getServerSideProps>) => (
     <>
         <Header />
         <Wrapper>
-            <div className="mt-4">
-                {post && author && comments !== null && likeCount !== null && isPostLiked !== null ? (
-                    <Post
-                        userId={userId}
-                        post={post}
-                        author={author}
-                        comments={comments}
-                        likeCount={likeCount}
-                        isLiked={isPostLiked}
-                        scheme="normal"
-                    />
-                ) : (
-                    <p>Post not found.</p>
-                )}
-            </div>
+            <div className="mt-4">{post ? <Post post={post} scheme="normal" /> : <p>Post not found.</p>}</div>
         </Wrapper>
     </>
 );
